df086b57fe52be8dbbcd431c0a0721e2a78c633c
Kiba Walenty <kibweecoin@gmail.com>
2014-06-13 18:59:44 +0200

    Initial Commit

---
+++ src/qt/test/uritests.cpp
@@ -0,0 +1,62 @@
+#include "uritests.h"
+#include "../guiutil.h"
+#include "../walletmodel.h"
+
+#include <QUrl>
+
+void URITests::uriTests()
+{
+    SendCoinsRecipient rv;
+    QUrl uri;
+    uri.setUrl(QString("kibweecoin:KLMD1gKGK44AGaRq5W8StG4LnhGDLjCfQh?req-dontexist="));
+    QVERIFY(!GUIUtil::parseBitcoinURI(uri, &rv));
+
+    uri.setUrl(QString("kibweecoin:KLMD1gKGK44AGaRq5W8StG4LnhGDLjCfQh?dontexist="));
+    QVERIFY(GUIUtil::parseBitcoinURI(uri, &rv));
+    QVERIFY(rv.address == QString("KLMD1gKGK44AGaRq5W8StG4LnhGDLjCfQh"));
+    QVERIFY(rv.label == QString());
+    QVERIFY(rv.amount == 0);
+
+    uri.setUrl(QString("kibweecoin:KLMD1gKGK44AGaRq5W8StG4LnhGDLjCfQh?label=Wikipedia Example Address"));
+    QVERIFY(GUIUtil::parseBitcoinURI(uri, &rv));
+    QVERIFY(rv.address == QString("KLMD1gKGK44AGaRq5W8StG4LnhGDLjCfQh"));
+    QVERIFY(rv.label == QString("Wikipedia Example Address"));
+    QVERIFY(rv.amount == 0);
+
+    uri.setUrl(QString("kibweecoin:KLMD1gKGK44AGaRq5W8StG4LnhGDLjCfQh?amount=0.001"));
+    QVERIFY(GUIUtil::parseBitcoinURI(uri, &rv));
+    QVERIFY(rv.address == QString("KLMD1gKGK44AGaRq5W8StG4LnhGDLjCfQh"));
+    QVERIFY(rv.label == QString());
+    QVERIFY(rv.amount == 100000);
+
+    uri.setUrl(QString("kibweecoin:KLMD1gKGK44AGaRq5W8StG4LnhGDLjCfQh?amount=1.001"));
+    QVERIFY(GUIUtil::parseBitcoinURI(uri, &rv));
+    QVERIFY(rv.address == QString("KLMD1gKGK44AGaRq5W8StG4LnhGDLjCfQh"));
+    QVERIFY(rv.label == QString());
+    QVERIFY(rv.amount == 100100000);
+
+    uri.setUrl(QString("kibweecoin:KLMD1gKGK44AGaRq5W8StG4LnhGDLjCfQh?amount=100&label=Wikipedia Example"));
+    QVERIFY(GUIUtil::parseBitcoinURI(uri, &rv));
+    QVERIFY(rv.address == QString("KLMD1gKGK44AGaRq5W8StG4LnhGDLjCfQh"));
+    QVERIFY(rv.amount == 10000000000LL);
+    QVERIFY(rv.label == QString("Wikipedia Example"));
+
+    uri.setUrl(QString("kibweecoin:KLMD1gKGK44AGaRq5W8StG4LnhGDLjCfQh?message=Wikipedia Example Address"));
+    QVERIFY(GUIUtil::parseBitcoinURI(uri, &rv));
+    QVERIFY(rv.address == QString("KLMD1gKGK44AGaRq5W8StG4LnhGDLjCfQh"));
+    QVERIFY(rv.label == QString());
+
+    QVERIFY(GUIUtil::parseBitcoinURI("kibweecoin://KLMD1gKGK44AGaRq5W8StG4LnhGDLjCfQh?message=Wikipedia Example Address", &rv));
+    QVERIFY(rv.address == QString("KLMD1gKGK44AGaRq5W8StG4LnhGDLjCfQh"));
+    QVERIFY(rv.label == QString());
+
+    // We currently don't implement the message parameter (ok, yea, we break spec...)
+    uri.setUrl(QString("kibweecoin:KLMD1gKGK44AGaRq5W8StG4LnhGDLjCfQh?req-message=Wikipedia Example Address"));
+    QVERIFY(!GUIUtil::parseBitcoinURI(uri, &rv));
+
+    uri.setUrl(QString("kibweecoin:KLMD1gKGK44AGaRq5W8StG4LnhGDLjCfQh?amount=1,000&label=Wikipedia Example"));
+    QVERIFY(!GUIUtil::parseBitcoinURI(uri, &rv));
+
+    uri.setUrl(QString("kibweecoin:KLMD1gKGK44AGaRq5W8StG4LnhGDLjCfQh?amount=1,000.0&label=Wikipedia Example"));
+    QVERIFY(!GUIUtil::parseBitcoinURI(uri, &rv));
+}
